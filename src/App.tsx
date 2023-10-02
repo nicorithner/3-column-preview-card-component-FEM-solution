096f1e4a093ae003811530bdd8db68710f8adabd
Nico Rithner <nicorithner@gmail.com>
2023-10-01 19:23:22 -0600

    Set up Column component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./App.scss";
 import { Column } from "./components/column/Column";
+import sedanIcon from "../public/images/icon-sedans.svg";
+import suvIcon from "../public/images/icon-suvs.svg";
+import luxuryIcon from "../public/images/icon-luxury.svg";
 
 function App() {
   return (
     <>
-      <Column />
+      <div className="columns-container">
+        <Column
+          icon={sedanIcon}
+          title="Sedan"
+          description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        />
+        <Column
+          icon={suvIcon}
+          title="SUVs"
+          description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+and off-road adventures. "
+        />
+        <Column
+          icon={luxuryIcon}
+          title="Luxury"
+          description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+  rental and arrive in style. "
+        />
+      </div>
     </>
   );
 }
--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,3 +1,19 @@
-export const Column = () => {
-  return <h1>I'm a Column!</h1>
-}
+type Props = {
+  icon: string;
+  title: string;
+  description: string;
+};
+export const Column = (props: Props) => {
+  return (
+    <>
+      <div className="column-container">
+        <div className="icon">
+          <img src={props.icon} alt="sedan icons" />
+        </div>
+        <div className="title">{props.title}</div>
+        <p className="description">{props.description} </p>
+        <button className="learn-more">Learn More</button>
+      </div>
+    </>
+  );
+};
